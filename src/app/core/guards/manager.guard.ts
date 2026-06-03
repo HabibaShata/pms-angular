6b76ccfd8676e13a79cebe3a7d6fb4826f76471b
esraaabuhalawa <esr.saad123@gmail.com>
2026-06-04 01:42:25 +0300

    Manager Module Starter Structure

--- src/app/core/guards/manager.guard.ts
+++ src/app/core/guards/manager.guard.ts
@@ -1,5 +1,5 @@
 import { CanActivateFn } from '@angular/router';
 
-export const employeeGuard: CanActivateFn = (route, state) => {
+export const managerGuard: CanActivateFn = (route, state) => {
   return true;
 };
--- src/app/features/dashboard/dashboard-routing.module.ts
+++ src/app/features/dashboard/dashboard-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 import { ChangePasswordComponent } from 'src/app/shared/components/change-password/change-password.component';
+import { managerGuard } from 'src/app/core/guards/manager.guard';
 
 const routes: Routes = [
   {
@@ -9,9 +10,13 @@ const routes: Routes = [
     component: DashboardComponent,
     children: [
       {
-        path:'',
-        redirectTo: 'employee',
+        path: '',
+        redirectTo: 'manager',
         pathMatch: 'full'
+      },
+      { path: 'manager',
+        canActivate: [managerGuard],
+        loadChildren: () => import('./manager/manager.module').then(m => m.ManagerModule)
       },
       { path: 'employee', loadChildren: () => import('./employee/employee.module').then(m => m.EmployeeModule) },
     ],
@@ -21,7 +26,6 @@ const routes: Routes = [
     component: ChangePasswordComponent,
     title: 'Change Password',
   },
-
 ];
 
 @NgModule({
