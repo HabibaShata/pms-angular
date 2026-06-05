9c57195c03b1264127342434b835c7af66b9543d
Nourhan-Nabil-9 <nnorhan535@gmail.com>
2026-06-06 01:38:47 +0300

    Add & Edit Projects

--- src/app/features/dashboard/manager/manager-routing.module.ts
+++ src/app/features/dashboard/manager/manager-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ManagerComponent } from './manager.component';
+import { AddEditProjectsComponent } from './projects/add-edit-projects/add-edit-projects.component';
 
 const routes: Routes = [
-  { path: '', component: ManagerComponent }];
+  { path: '', component: ManagerComponent },
+  {
+    path: 'projects',
+    loadChildren: () =>
+      import('./projects/projects.module').then((m) => m.ProjectsModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ManagerRoutingModule { }
+export class ManagerRoutingModule {}
--- src/app/features/dashboard/manager/services/manager.service.ts
+++ src/app/features/dashboard/manager/services/manager.service.ts
@@ -1,10 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ITasksCount, IUserscount } from '../interfaces/manger.interface';
+import {
+  IProject,
+  IProjectPayload,
+  ITasksCount,
+  IUserscount,
+} from '../interfaces/manger.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ManagerService {
   private http = inject(HttpClient);
@@ -15,5 +20,15 @@ export class ManagerService {
 
   getTasksCount(): Observable<ITasksCount> {
     return this.http.get<ITasksCount>('Task/count');
+  }
+  createProject(data: IProjectPayload): Observable<IProject> {
+    return this.http.post<IProject>('Project', data);
+  }
+  getProjectById(id: number): Observable<IProject> {
+    return this.http.get<IProject>(`Project/${id}`);
+  }
+
+  updateproject(id: number, data: IProjectPayload): Observable<IProject> {
+    return this.http.put<IProject>(`Project/${id}`, data);
   }
 }
