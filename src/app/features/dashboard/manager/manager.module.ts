7c53e10671b327773555074391e15ffcf7e9100d
Mariam <MariamSabry1@GitHub>
2026-06-08 21:39:21 +0300

    add-edit (import)

--- src/app/features/dashboard/manager/manager.module.ts
+++ src/app/features/dashboard/manager/manager.module.ts
@@ -5,12 +5,14 @@ import { ManagerRoutingModule } from './manager-routing.module';
 import { ManagerComponent } from './manager.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { A11yModule } from "@angular/cdk/a11y";
+import { AddEditProjectsComponent } from './projects/components/add-edit-projects/add-edit-projects.component';
 
 
 @NgModule({
   declarations: [
     ManagerComponent,
- 
+    AddEditProjectsComponent
+
   ],
   imports: [
     SharedModule,
